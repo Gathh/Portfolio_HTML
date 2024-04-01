300f959366df515377589168fa4d2fd8861ec64c
Rafagath <125030016+Gathh@users.noreply.github.com>
2024-04-01 10:06:32 -0300

    Add files via upload

---
+++ Portfólio/script.js
@@ -0,0 +1,14 @@
+function funcaoTec() {
+  console.log("Botão 1 foi clicado!");
+  window.location.assign('Tecnico/tecnico.html');
+}
+
+function funcaoMaterias() {
+  console.log("Botão 2 foi clicado!");
+  window.location.assign('Materias/materias.html');
+}
+
+function funcaoSobre() {
+  console.log("Botão 3 foi clicado!");
+  window.location.assign('Sobre/sobre.html');
+}
