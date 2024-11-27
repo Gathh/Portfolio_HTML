2c91a5e73fca9fbdce4ca51983f3a09e36c25122
Gathh <rafagmk@gmail.com>
2024-11-27 18:28:21 -0300

    arrumei a formatacao

--- Materias/materias.js
+++ Materias/materias.js
@@ -1,68 +1,68 @@
-  function funcaoHome() {
-    console.log("Botão 1 foi clicado!");
-    window.location.href="../index.html"
-  }
+function funcaoHome() {
+  console.log("Botão 1 foi clicado!");
+  window.location.href = "../index.html"
+}
 
-  function funcaoTec() {
-    console.log("Botão 2 foi clicado!");
-    window.location.href="../Tecnico/tecnico.html"
-  }
+function funcaoTec() {
+  console.log("Botão 2 foi clicado!");
+  window.location.href = "../Tecnico/tecnico.html"
+}
 
-  function funcaoSobre() {
-    console.log("Botão 3 foi clicado!");
-    window.location.href="../Sobre/sobre.html" 
-  }
+function funcaoSobre() {
+  console.log("Botão 3 foi clicado!");
+  window.location.href = "../Sobre/sobre.html"
+}
 
-  function funcaoMtm() {
-    console.log("Botão 4 foi clicado!");
-    window.location.href="./paginas/matematica.html"
-  }
-  function funcaoNat() {
-    console.log("Botão 5 foi clicado!");
-    window.location.href="./paginas/natureza.html"
-  }
+function funcaoMtm() {
+  console.log("Botão 4 foi clicado!");
+  window.location.href = "./paginas/matematica.html"
+}
+function funcaoNat() {
+  console.log("Botão 5 foi clicado!");
+  window.location.href = "./paginas/natureza.html"
+}
 
-  function funcaoLing() {
-    console.log("Botão 6 foi clicado!");
-    window.location.href="./paginas/linguagens.html"
-  }
+function funcaoLing() {
+  console.log("Botão 6 foi clicado!");
+  window.location.href = "./paginas/linguagens.html"
+}
 
-  function funcaoHum() {
-    console.log("Botão 7 foi clicado!");
-    window.location.href="./paginas/historia.html"
-  }
+function funcaoHum() {
+  console.log("Botão 7 foi clicado!");
+  window.location.href = "./paginas/historia.html"
+}
 
-  function funcaoGithub(){
-    console.log("Botão 4 foi clicado!")
-    window.open("https://github.com/Gathh") 
-  }
-  
-  function funcaoLinkedin(){
-    console.log("Botão 5 foi clicado!")
-    window.open("https://www.linkedin.com/in/rafagath-klug-11122a269/")
-  } 
+function funcaoGithub() {
+  console.log("Botão 4 foi clicado!")
+  window.open("https://github.com/Gathh")
+}
 
-  function funcaoVoltar() {
-    console.log("Botão 8 foi clicado!");
-    window.location.href="materias.html" 
-  }
-  
-  function funcaoVoltar2() {
-    console.log("Botão 8 foi clicado!");
-    window.location.href="../materias1.html" 
-  }
+function funcaoLinkedin() {
+  console.log("Botão 5 foi clicado!")
+  window.open("https://www.linkedin.com/in/rafagath-klug-11122a269/")
+}
 
-  function funcaoPPE() {
-    console.log("Botão 9 foi clicado!");
-    window.location.href="../PPE/ppe.html" 
-  }
+function funcaoVoltar() {
+  console.log("Botão 8 foi clicado!");
+  window.location.href = "materias.html"
+}
 
-  function funcaoPPE() {
-    console.log("Botão 9 foi clicado!");
-    window.location.href="../PPE/ppe.html" 
-  }
+function funcaoVoltar2() {
+  console.log("Botão 8 foi clicado!");
+  window.location.href = "../materias1.html"
+}
 
-  function funcao1() {
-    console.log("Botão 9 foi clicado!");
-    window.location.href="materias1.html" 
-  }
+function funcaoPPE() {
+  console.log("Botão 9 foi clicado!");
+  window.location.href = "../PPE/ppe.html"
+}
+
+function funcaoPPE() {
+  console.log("Botão 9 foi clicado!");
+  window.location.href = "../PPE/ppe.html"
+}
+
+function funcao1() {
+  console.log("Botão 9 foi clicado!");
+  window.location.href = "materias1.html"
+}
